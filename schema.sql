137a18bff20a3d4ef2434a4b5b71298395c3ddca
OkoyeCharles <okoyecharles509@gmail.com>
2022-09-12 11:46:34 +0100

    Query specific data

--- schema.sql
+++ schema.sql
@@ -15,3 +15,12 @@ INSERT INTO animals VALUES (1, 'Agumon', '2020-02-03', 0, TRUE, 10.23);
 INSERT INTO animals VALUES (2, 'Gabumon', '2018-11-15', 2, TRUE, 8.0);
 INSERT INTO animals VALUES (3, 'Pikachu', '2021-01-07', 1, FALSE, 15.04);
 INSERT INTO animals VALUES (4, 'Devimon', '2017-05-12', 5, TRUE, 11.0);
+
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = TRUE;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;

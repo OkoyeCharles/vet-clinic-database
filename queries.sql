5a4debe500774e38d579a39d1767b0f8077d8388
OkoyeCharles <okoyecharles509@gmail.com>
2022-09-13 12:11:59 +0100

    Write queries for answering questions

--- queries.sql
+++ queries.sql
@@ -1,5 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+-- QUERIES (1)
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
 SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
@@ -55,3 +56,28 @@ WHERE weight_kg < 0;
 
 COMMIT;
 -----
+
+
+-- QUERIES (2)
+-- How many animals are there?
+SELECT COUNT(*) FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT * FROM animals 
+WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, MAX(escape_attempts) FROM animals 
+GROUP BY neutered;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals
+GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;

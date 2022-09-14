ec806c1560dc87804a74b63210cce95c4d1cbb54
OkoyeCharles <okoyecharles509@gmail.com>
2022-09-14 10:19:24 +0100

    Insert data into owners table

--- data.sql
+++ data.sql
@@ -11,3 +11,13 @@ INSERT INTO animals VALUES(8, 'Angemon', '2005-06-12', 1, TRUE, -45.0);
 INSERT INTO animals VALUES(9, 'Boarmon', '2005-06-07', 7, TRUE, 20.4);
 INSERT INTO animals VALUES(10, 'Blossom', '1998-10-13', 3, TRUE, 17.0);
 INSERT INTO animals VALUES(11, 'Ditto', '2022-05-14', 4, TRUE, 22.0);
+
+INSERT INTO OWNERS
+  (full_name, age)
+VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
